cea480bbbff81c5cd7b8a544d1532e9e0e578c1a
Jonathan Chan <johnkchan252@gmail.com>
2018-08-29 02:29:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,8 +10,8 @@ class Application
  
       item_name = req.path.split("/items/").last
       
-      if @@items.find{|i| i.title == item_name}
-        item = @@items.find{|i| i.title == item_name}
+      if @@items.find{|i| i.name == item_name}
+        item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else
         resp.write "Item not found"
